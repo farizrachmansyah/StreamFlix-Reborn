1d0cd2800d2a33a53efee9f25348f15120e6211c
Fariz Rachmansyah <farizrachmansyah7@gmail.com>
2021-03-09 15:06:03 +0700

    menyelesaikan semuanya: request api untuk cast, membuat element htmlnya, menggabungkan ke parent element

--- app/js/app.js
+++ app/js/app.js
@@ -15,11 +15,13 @@ if (document.title == 'StreamFlix | Best movie shop on planet') {
     const selectedMovieID = localStorage.getItem('movieid');
     const selectedMoviePrice = localStorage.getItem('movieprice')
     const detailMoviesData = await getDetailMovies(selectedMovieID);
-    const backdropData = await getBackdrop(detailMoviesData);
+    const castData = await getMovieActors(selectedMovieID);
+    const backdropData = getBackdrop(detailMoviesData);
+    const genreData = getGenre(detailMoviesData);
+    const yearData = getYearRelease(detailMoviesData);
 
-    console.log(detailMoviesData);
     // update ui
-    updateDetailsUI(selectedMoviePrice, detailMoviesData, backdropData);
+    updateDetailsUI(selectedMoviePrice, detailMoviesData, castData, backdropData, genreData, yearData);
   });
 }
 
@@ -95,9 +97,20 @@ function cardClicked() {
   document.addEventListener('click', function (e) {
     // jika yang di klik adalah element dengan class tertentu
     if (e.target.classList.contains('card-poster')) {
-      const buttonPrice = e.target.nextElementSibling;
+      const priceButton = e.target.nextElementSibling;
       const movieID = e.target.dataset.movieid;
-      const moviePrice = buttonPrice.dataset.strprice;
+      const moviePrice = priceButton.dataset.strprice;
+
+      localStorage.setItem('movieid', movieID);
+      localStorage.setItem('movieprice', moviePrice);
+    } else if (e.target.classList.contains('card-title')) {
+      // kalo yang di klik card-title, ambil element card-poster sama button price dulu
+      const movieCard = e.target.parentNode;  // a tag
+      const priceButton = movieCard.nextElementSibling; // button tag
+
+      // abis itu ambil datasetnya
+      const movieID = movieCard.dataset.movieid;
+      const moviePrice = priceButton.dataset.strprice;
 
       localStorage.setItem('movieid', movieID);
       localStorage.setItem('movieprice', moviePrice);
@@ -114,6 +127,18 @@ function getDetailMovies(movieid) {
     .then(res => res);
 }
 
+function getMovieActors(movieid) {
+  const CAST_URL = `https://api.themoviedb.org/3/movie/${movieid}/credits?api_key=c1042292167adc9dc2dbe3a920a743d2&language=en-US`;
+
+  return fetch(CAST_URL)
+    .then(res => res.json())
+    .then(res => res.cast)
+    .then(res => {
+      const castName = res.map(cast => cast.name);
+      return castName;
+    });
+};
+
 function getBackdrop(selectedmovie) {
   const BACKDROP_URL = 'https://image.tmdb.org/t/p/original';
 
@@ -122,8 +147,65 @@ function getBackdrop(selectedmovie) {
   return backdropLink;
 }
 
-function updateDetailsUI(price, detailmovie, backdrop) {
-  // console.log(movies);
+function getGenre(detailmovie) {
+  const genreList = detailmovie.genres.map(genre => {
+    return genre.name;
+  });
+
+  return genreList;
+}
+
+function getYearRelease(detailmovie) {
+  const releaseDate = detailmovie.release_date;
+  const yearDate = releaseDate.substring(0, 4);
+
+  return yearDate;
+}
+
+function updateDetailsUI(price, detailmovie, cast, backdrop, genrelist, yeardate) {
+  const detailsMain = document.querySelector('.details-main');
+
+  const contentElements = `
+    <div class="details-main__backdrop"></div>
+    <div class="details-main__content container--px">
+      <h1 class="details-main__content__title">${detailmovie.title}</h1>
+      <div class="details-main__content__about">
+        <h2 class="details-main__content__about__price">Rp ${price}</h2>
+        <div class="details-main__content__about__info">
+          <span class="info-rating">${detailmovie.vote_average}</span>
+          <span class="info-spacer">|</span>
+          <span class="info-year">${yeardate}</span>
+          <span class="info-spacer">|</span>
+          <span class="info-genre">${genrelist.join(', ')}</span>
+        </div>
+        <p class="details-main__content__about__overview">
+          ${detailmovie.overview}
+        </p>
+        <div class="details-main__content__about__credits">
+          <p class="credits-cast"><span>Starring:</span> ${cast.join(', ')}</p>
+        </div>
+      </div>
+      <div class="details-main__content__button">
+        <button class="button-watch">watch</button>
+        <button class="button-buy">buy</button>
+      </div>
+    </div>
+  `;
+
+  detailsMain.innerHTML = contentElements;
+
+  setBackdrop(backdrop);
+}
+
+function setBackdrop(backdropImage) {
+  const detailsBackdrop = document.querySelector('.details-main__backdrop');
+  const detailsContent = document.querySelector('.details-main__content');
+
+  if (window.innerWidth < 1025) {
+    detailsBackdrop.style.backgroundImage = `url(${backdropImage})`;
+  } else {
+    detailsContent.style.backgroundImage = `url(${backdropImage})`;
+  }
 }
 
 // ANOTHER STUFF -------------------------------
